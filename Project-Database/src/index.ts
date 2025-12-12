822c8f3ae5feebc3472eed31c2f6aae55b96f6d9
Rizqi Setiawan <seiawanrizqi@gmail.com>
2025-12-12 07:35:29 +0700

    feat: Week2-Day10

--- Project-Database/src/index.ts
+++
@@ -1,7 +0,0 @@
-import app from "./app"
-import config from './utils/env'
-
-app.listen(config.PORT, () => {
-    console.log(`Server running at ${config.HOST}:${config.PORT}`)
-})
-
--- Project-Database/src/utils/env.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-export default {
-    HOST: process.env.HOST || 'localhost',
-    PORT: process.env.PORT || 5000,
-    NODE_ENV: process.env.NODE_ENV,
-} as const
